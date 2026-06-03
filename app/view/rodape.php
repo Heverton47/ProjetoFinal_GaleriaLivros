20916506fb80d64fe32b0012223d2b3c966f866d
Heverton <hsuchek47@gmail.com>
2026-06-02 21:58:58 -0300

    Desenvolvimento de layout geral

--- app/view/rodape.php
+++ app/view/rodape.php
@@ -1,2 +1,19 @@
-ESTA PROVAVELMENTE SERIA A PRÓXIMA PARTE FEITA, COM O NOME, REDES SOCIAIS E O LOGO E TALVEZ UM ATALHO PARA A HOME(ONDE EU IMAGINO TENDO INFORMAÇÕES SOBRE O PROJETO, 
-COMO ELE FUNCIONA E QUEM SÃO OS AUTORES(NÓS)). 
+<!--ESTA PROVAVELMENTE SERIA A PRÓXIMA PARTE FEITA, COM O NOME, REDES SOCIAIS E O LOGO E TALVEZ UM ATALHO PARA A HOME(ONDE EU IMAGINO TENDO INFORMAÇÕES SOBRE O PROJETO, 
+COMO ELE FUNCIONA E QUEM SÃO OS AUTORES(NÓS)).-->
+<footer class="bg-dark text-white p-3 bottom-0 mt-4">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6 text-start">
+        <p class="mb-1">Desenvolvido por: Heverton Ricardo e Caio Lamers</p>
+        <p class="mb-0">2026</p>
+      </div>
+
+      <div class="col-md-6 text-end">
+        <h5>Redes Sociais</h5>
+        <a href="#" class=""><img class="icon_rede" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1280px-Instagram_logo_2022.svg.png" alt="Instagram"></a>
+        <a href="#" class=""><img class="icon_rede" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1280px-Facebook_f_logo_%282019%29.svg.png" alt="Facebook"></a>
+        <a href="#" class=""><img class="icon_rede" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/1280px-Logo_of_Twitter.svg.png" alt="Twitter"></a>
+      </div>
+    </div>
+  </div>
+</footer> 
--- index.php
+++ index.php
@@ -3,14 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Galeria de Livros</title>
-    <link rel="icon" href="https://cdn.pixabay.com/photo/2016/09/16/09/20/books-1673578_1280.png" type="image/x-icon">
+    <link rel="icon" href="https://cdn.pixabay.com/photo/2016/09/16/09/20/books-1673578_1280.png" type="image/png">
     <link rel="stylesheet" href="css/estilo.css">
-
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <title>Galeria de Livros</title>
 </head>
 <body>
-    <h1>Galeria de Livros</h1>
-    <p>Bem-vindo à nossa galeria de livros!</p>
-    <a href="app/view/lista_livros.php">Ver livros</a>
+
+<div class="container-fluid">
+    <div class="row">
+
+    <div class="col-2 coluna_preta d-none d-md-block"></div>
+
+    <div class="col-8 conteudo_index">
+        
+    <h1 class="text-white">Galeria de Livros</h1><br>
+    <h4 class="text-white">Bem-vindo à nossa pequena galeria de livros!</h4><br>
+    <a href="app/view/home.php" class="btn btn-primary btn-lg text-white">Sobre nós</a><br><br>
+    <a href="app/view/lista_livros.php" class="btn btn-primary btn-lg text-white">Ver livros</a><br><br>   
+    <a href="app/view/login.php" class="btn btn-primary btn-lg text-white">Login/Cadastro</a><br><br>
+        <?php if (isset($_SESSION['usuario'])): ?>
+            <a href="" class="btn btn-primary btn-lg text-white">CADASTRAR</a><br><br>
+            <a href="" class="btn btn-danger btn-lg text-white">LOGOUT</a>
+        <?php endif; ?>
+    </div>
+
+    <div class="col-2 coluna_preta d-none d-md-block"></div>
+    </div>
+</div>
+
 </body>
 </html>
